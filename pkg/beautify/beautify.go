9d317e2fcd154df3305116e6e4407277891747fb
kesonan <anqiansong@gmail.com>
2024-07-04 09:24:35 +0800

    Add beautify

---
+++ pkg/beautify/beautify.go
@@ -0,0 +1,61 @@
+package beautify
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/kesonan/xlib/pkg/converter/constx"
+	"github.com/pelletier/go-toml/v2"
+	"gopkg.in/yaml.v3"
+)
+
+func JSON(s string) string {
+	var v any
+	err := json.Unmarshal([]byte(s), &v)
+	if err != nil {
+		return s
+	}
+	writer := bytes.NewBuffer(nil)
+	encoder := json.NewEncoder(writer)
+	encoder.SetIndent("", constx.Indent)
+	encoder.SetEscapeHTML(true)
+	err = encoder.Encode(v)
+	if err != nil {
+		return s
+	}
+	return writer.String()
+}
+
+func YAML(s string) string {
+	var v any
+	err := yaml.Unmarshal([]byte(s), &v)
+	if err != nil {
+		return s
+	}
+	writer := bytes.NewBuffer(nil)
+	encoder := yaml.NewEncoder(writer)
+	defer encoder.Close()
+	encoder.SetIndent(2)
+	err = encoder.Encode(v)
+	if err != nil {
+		return s
+	}
+	return writer.String()
+}
+
+func TOML(s string) string {
+	var v any
+	err := toml.Unmarshal([]byte(s), &v)
+	if err != nil {
+		return s
+	}
+	writer := bytes.NewBuffer(nil)
+	encoder := toml.NewEncoder(writer)
+	encoder.SetIndentSymbol(constx.Indent)
+	encoder.SetIndentTables(true)
+	encoder.SetMarshalJsonNumbers(true)
+	err = encoder.Encode(v)
+	if err != nil {
+		return ""
+	}
+	return writer.String()
+}
